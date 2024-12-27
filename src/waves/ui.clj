29d0346f8f9a9286c782b627c4326a61cd9376a0
Nicolas Modrzyk <hellonico@gmail.com>
2024-12-27 17:40:07 +0900

    using latest pyjama

--- src/waves/ui.clj
+++ src/waves/ui.clj
@@ -4,6 +4,7 @@
   (:require [cljfx.api :as fx]
             [clojure.java.io :as io]
             [waves.core]
+            [pyjama.state]
             [waves.core2]))
 
 (def app-state
@@ -16,17 +17,6 @@
          ;:system-prompt "You are a machine translator from Japanese to English. You are given one string each time. When the string is in English you return the string as is. If the string is in Japanese, you answer with the best translation. Your answer will only contain the translation and the translation only, nothing else, no question, no explanation. If you do not know, answer with the same string as input"
          :prompt-template "You are a machine translator from Japanese to English. You are given one string each time. When the string is in English you return the string as is. If the string is in Japanese, you answer with the best translation. Your answer will only contain the translation and the translation only, nothing else, no question, no explanation. If you do not know, answer with the same string as input. Translate: %s"
          :status :idle})) ; :idle, :running, or :completed
-
-;; Fetch models from the URL
-(defn fetch-models [url on-complete]
-  (pyjama.core/ollama url :tags {}
-                      (fn [res] (on-complete (map :name (:models res))))))
-
-;; When the URL changes, refresh the model list
-(defn update-model-list [url]
-  (fetch-models url
-                (fn [models]
-                  (swap! app-state assoc :models models))))
 
 (defn file-chooser []
   (let [chooser (javafx.stage.FileChooser.)]
@@ -48,21 +38,6 @@
 (def failed-image
   (Image. (io/input-stream (io/resource "failed.png"))))
 
-;; Simulate long-running task
-(defn run-task [file-path on-complete]
-  (let [f (future
-    (try
-      ;(Thread/sleep 5000) ; Simulating long-running task
-      (long-running-task)
-      (let [output-file-path (waves.utils/compute-output-file-path file-path)]
-        (on-complete :success output-file-path))
-      (catch Exception e
-        (on-complete :failure nil))))]
-    ))
-
-;; Stop task
-(defonce task-stop-flag (atom false))
-
 (defn root-view [state]
   {:fx/type :stage
    :title "PPTX Translator"
@@ -76,12 +51,6 @@
                   :spacing 10
                   :padding 20
                   :children [
-                             ;{:fx/type :menu-bar
-                             ; :menus [{:fx/type :menu
-                             ;          :text "File  "
-                             ;          :items [{:fx/type :menu-item
-                             ;                   :text "Quit"
-                             ;                   :on-action (fn [_] (System/exit 0))}]}]}
                              {:fx/type :label
                               :text "PPTX Translator"}
                              {:fx/type :label
@@ -101,23 +70,13 @@
                               :text (:url state)
                               :on-text-changed #(do
                                                   (swap! app-state assoc :url %)
-                                                  (update-model-list %))}
-                             ;{:fx/type :label :text "Model"}
-                             ;{:fx/type :text-field
-                             ; :text (:model state)
-                             ; :on-text-changed #(swap! app-state assoc :model %)}
+                                                  (pyjama.state/local-models app-state))}
                              {:fx/type :label :text "Model"}
                              {:fx/type :combo-box
-                              :items (:models state)
+                              :items (:local-models state)
                               :value (:model state)
                               :on-action #(swap! app-state assoc :model (.. % -newValue))}
 
-                             ;{:fx/type :label :text "System Prompt"}
-                             ;{:fx/type :text-area
-                             ; :wrap-text true
-                             ; :pref-height 100
-                             ; :text (:system-prompt state)
-                             ; :on-text-changed #(swap! app-state assoc :system-prompt %)}
                              {:fx/type :label :text "Prompt Template"}
                              {:fx/type :text-area
                               :wrap-text true
@@ -174,5 +133,5 @@
     :opts {:app-state app-state}))
 
 (defn -main [& args]
-  (update-model-list (:url @app-state))
+  (pyjama.state/local-models app-state)
   (fx/mount-renderer app-state renderer))
